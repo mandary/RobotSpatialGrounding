850e6401609768f9d182c07f630349bac0bd5e19
mandary <mandary@github.com>
2016-02-23 01:39:40 -0800

    partly updated

--- intro/Dijkstra.m
+++ intro/Dijkstra.m
@@ -1,58 +1,53 @@
 % Dijkstra in costmap 
 %function [xp, yp, cost] = 
 function Dijkstra(costmap, start, goal, proportion)
-    cosGraph = createGraph(costmap);
+    %cosGraph = createGraph(costmap);
     xp = [];
     yp = [];
-    working = [];
+    working = table();
     hi = size(costmap, 1);
     wi = size(costmap, 2);
-    visited = [];
-    cost(1:hi, 1:wi) = 0;
+    visited(1:hi, 1:wi) = 0;
+    cost(1:hi, 1:wi) = Inf;
+    path;
+    
     % find start and goal
     i = start{1, 2} + proportion + 1;
     j = start{1, 1} + proportion + 1;
-    temps = strcat(num2str(i), '0', num2str(j));
-    temps = str2double(temps);
+%     temps = strcat(num2str(i), '0', num2str(j));
+%     temps = str2double(temps);
     gis = goal{1, 2} - proportion;
     gjs = goal{1, 1} - proportion;
-    tempe = strcat(num2str(gis), '0', num2str(gjs));
-    tempe = str2double(tempe);
-    working = [working temps];
+%     tempe = strcat(num2str(gis), '0', num2str(gjs));
+%     tempe = str2double(tempe);
+    working = [working; table(i, j)];
     
     % find path process
     while size(working, 1) ~= 0
-        tempn = working{size(working, 1), 1};
+        tempi = working{size(working, 1), 1};
+        tempj = working{size(working, 1), 2};
         working(size(working, 1),:) = []; % conventional pop
         % find the goal
-        if tempn == tempe
+        if tempi == gis && tempj == gjs
             break;
         end
         % not visited yet
-        if ~sum(ismember(visited, tempn))
-            visited = [visited tempn];
+        if ~visited(tempi, tempj)
+            visited(tempi, tempj) = 1;
             % for the visited's neighbor
-            mincost = Inf;
-            mini = tempi;
-            minj = tempj;
-            for neighbor = neighbors(cosGraph, tempn)                 
-                if (cost(i, j) == 0) || (cost(i, j) > cost(tempi, tempj) + costmap(i, j))
-                    cost(i, j) = round(cost(tempi, tempj) + costmap(i, j));
-                    if costmap(i, j) < mincost
-                        mincost = cost(i, j);
-                        mini = i;
-                        minj = j;
+            for i = tempi-1:tempi+1
+                for j = tempj-1:tempj+1
+                    if i ~= tempi && j ~= tempj && ~visited(i, j) % no self loop
+                        working = [working; table(i, j)];
+                        c1 = cost(tempi, tempj) + costmap(i, j);
+                        c2 = cost(i, j);
+                        if c1 < c2
+                            cost(i, j) = c1;
+                            path([i, j]) = path([tempi, tempj]);
+                        end
                     end
                 end
-            end
-            % put smallest cost neighbor into path
-            if ~visited(mini, minj)
-                i = mini;
-                j = minj;
-                working = [working;table(i, j)];
-                xp = [xp minj];
-                yp = [yp mini];
-            end           
+            end         
         end
     end
     disp(path);
