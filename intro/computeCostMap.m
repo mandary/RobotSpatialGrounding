d0fd5cb20c52a750fd5ea43d8416225f2c009dd9
mandary <mandary@github.com>
2016-03-01 12:58:23 -0800

    exp the cost

--- intro/computeCostMap.m
+++ intro/computeCostMap.m
@@ -3,12 +3,8 @@ function result = computeCostMap(width, height, weights, featuremap)
 %     result(1:height, 1:width) = 0;
 %     for i = 1:height
 %         for j = 1:width
-%             if matrix(i, j) == 10000 % obstacle
-%                 result(i, j) = 10000;
-%             elseif matrix(i, j) == 10 % goal
-%                 result(i, j) = 10;
-%             elseif matrix(i, j) == 20 % start
-%                 result(i, j) = 20;
+%             if matrix(i, j) == -100 % obstacle
+%                 result(i, j) = -100;
 %             else
 %                 feature = computeFeature(j, i, goal, object); %pass in feature map
 %                 result(i, j) = sum(feature.*weights);
@@ -23,7 +19,7 @@ function result = computeCostMap(width, height, weights, featuremap)
             for k = 1:4
                 sum = sum + featuremap(i, j, k) * weights(k);
             end
-            result(i, j) = sum;
+            result(i, j) = exp(sum);
         end
     end
 end
