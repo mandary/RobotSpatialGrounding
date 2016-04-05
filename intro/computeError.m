9a153f4fd5c9d83ba10ef595179a613d42feb6a9
mandary <mandary@github.com>
2016-04-05 00:30:17 -0700

    error fixed to be exponential to ensure + cost

--- intro/computeError.m
+++ intro/computeError.m
@@ -1,22 +1,20 @@
 % update weights for cost function
+% take in weights, planned path feature list, sample path feature list, 
+% learning rate, convex optimization rate(lambda)
 function updatedWeights = computeError(weights, pfeat, sfeat, lr, lambda)
     updatedWeights = weights;
+    weightn = size(weights, 2);
     siz = min(size(sfeat, 2), size(pfeat, 2));
     for i = 1:siz
-        index = mod(i, 4);
+        index = mod(i, weightn);
         if index == 0
-            index = 4;
+            index = weightn;
         end
-        updatedWeights(index) = updatedWeights(index) + lr * ((sfeat(i) - pfeat(i)) + lambda * updatedWeights(index));
+        updatedWeights(index) = updatedWeights(index) * exp(-lr * ((sfeat(i) - pfeat(i)) + lambda * updatedWeights(index)));
         if updatedWeights(index) >= 200
             updatedWeights(index) = 200;
         end
-%         if updatedWeights(index) < 0
-%             updatedWeights(index) = 0;
-%         end
         
     end
         
 end
-
-% might want considering normalize
