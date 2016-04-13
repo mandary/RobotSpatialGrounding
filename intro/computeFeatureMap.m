304640d64c178c4072b54859433b2ad2e7d598c4
mandary <mandary@github.com>
2016-04-13 13:10:48 -0700

    fixed negative cost bug

--- intro/computeFeatureMap.m
+++ intro/computeFeatureMap.m
@@ -1,44 +1,22 @@
 % compute feature map for map, each feature value is normalized from 0 ~ 1
 function result = computeFeatureMap(width, height, goal, object, featn)
-    % collect maxmin of feature value for normalizing
-    maxmin = [];
-    for g = 1:featn*2
-        if mod(g, 2) == 1
-            maxmin = [maxmin 0];
-        else
-            maxmin = [maxmin Inf];
-        end
-    end
     % initialize feature map
-    result(1:height, 1:width, 1:featn + 1) = 0;
+    result = zeros(height, width, featn + 1);
     % compute feature for each square on the map
     for i = 1:height
         for j = 1:width
-            feature = computeFeature(j, i, goal, object); 
-            for k = 1:featn
-                % store computed feature
-                result(i, j, k) = feature(k);
-                % collect max and min for each feature value
-                if feature(k) > maxmin(k * 2 - 1)
-                    maxmin(k * 2 - 1) = feature(k);
-                end
-                if feature(k) < maxmin(k * 2)
-                    maxmin(k * 2) = feature(k);
-                end
-            end
-            % Constant feature for number of steps
-            result(i, j, featn + 1) = feature(featn + 1);
+            result(i,j,:) = computeFeature(j, i, goal, object); 
         end
     end
+    % Max and min of features
+    maxval = max(reshape(result, height*width, featn+1), [], 1);
+    minval = min(reshape(result, height*width, featn+1), [], 1);
     
     % normalize feature value from 0 ~ 1
     for i = 1:height
         for j = 1:width
-            feature = computeFeature(j, i, goal, object); 
             for k = 1:featn
-                min = maxmin(k * 2);
-                diff = maxmin(k * 2 - 1) - min;
-                result(i, j, k) = (feature(k) - min) / diff;
+                result(i, j, k) = (result(i,j,k) - minval(k)) / (maxval(k) - minval(k));
             end
         end
     end
--- intro/computeLossMap.m
+++ intro/computeLossMap.m
@@ -1,8 +1,6 @@
 % compute loss map for desired path
 function lossmap = computeLossMap(width, height, x, y)
     lossmap(1:height, 1:width) = 0;
-    max = 0;
-    min = Inf;
     % compute euclidean loss
     for i = 1:height
         for j = 1:width
@@ -12,22 +10,9 @@ function lossmap = computeLossMap(width, height, x, y)
                 if dist < mdist
                     mdist = dist;
                 end
-                if dist < min
-                    min = dist;
-                end
-                if dist > max
-                    max = dist;
-                end
             end
             lossmap(i, j) = mdist;
         end
     end
-    diff = max - min;
-    % normalize loss field from 0 ~ 1
-    for i = 1:height
-        for j = 1:width
-            value = lossmap(i, j);
-            lossmap(i, j) = (value - min) / diff;
-        end
-    end
+    lossmap = (lossmap - min(lossmap(:))) / (max(lossmap(:)) - min(lossmap(:)));
 end
