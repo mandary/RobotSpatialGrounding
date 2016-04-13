d3c0e39fe8b07c2af61e61bf62b0d6ccfe0e92cf
mandary <mandary@github.com>
2016-04-13 14:45:46 -0700

    partial working

--- intro/Dijkstra.m
+++ intro/Dijkstra.m
@@ -1,59 +1,83 @@
 % Dijkstra in costmap 
-%function [xp, yp, cost] = 
-function Dijkstra(costmap, start, goal, proportion)
-    %cosGraph = createGraph(costmap);
-    xp = [];
-    yp = [];
+function [xp, yp] = Dijkstra(costmap, start, goal, proportion)
     working = table();
     hi = size(costmap, 1);
     wi = size(costmap, 2);
     visited(1:hi, 1:wi) = 0;
     cost(1:hi, 1:wi) = Inf;
-    path;
-    
-    % find start and goal
-    i = start{1, 2} + proportion + 1;
-    j = start{1, 1} + proportion + 1;
-%     temps = strcat(num2str(i), '0', num2str(j));
-%     temps = str2double(temps);
-    gis = goal{1, 2} - proportion;
-    gjs = goal{1, 1} - proportion;
-%     tempe = strcat(num2str(gis), '0', num2str(gjs));
-%     tempe = str2double(tempe);
-    working = [working; table(i, j)];
+    path(1:hi, 1:wi, 1:2*hi) = 0;
+    index(1:hi, 1:wi) = 0;
+    sy = start{1, 2};
+    sx = start{1, 1};
+    y = sy;
+    x = sx;
+    working = [working; table(y, x)];
+    path(sy, sx, 1) = sy;
+    path(sy, sx, 2) = sx;
+
+    index(sy, sx) = 1;
+    cost(sy, sx) = costmap(sy, sx);
+    xp = [];
+    yp = [];
     
     % find path process
     while size(working, 1) ~= 0
-        tempi = working{size(working, 1), 1};
-        tempj = working{size(working, 1), 2};
-        working(size(working, 1),:) = []; % conventional pop
+        % traverse working table, pop the smallest cost element
+        tempy = 0;
+        tempx = 0;
+        mincost = Inf;
+        minindex = 1;
+        for i = 1:size(working, 1)
+            if costmap(working{i, 1}, working{i, 2}) < mincost
+                tempy = working{i, 1};
+                tempx = working{i, 2};
+                mincost = costmap(tempy, tempx);
+                minindex = i;
+            end
+        end
+        % conventional pop
+        working(minindex, :) = [];
         % find the goal
-        if tempi == gis && tempj == gjs
+        if isGoal(tempx, tempy, goal, proportion)
+            for i = 1:index(tempy, tempx)
+                yp = [yp path(tempy, tempx, i*2-1)];
+                xp = [xp path(tempy, tempx, i*2)];
+            end
             break;
         end
         % not visited yet
-        if ~visited(tempi, tempj)
-            visited(tempi, tempj) = 1;
-            % for the visited's neighbor
-            for i = tempi-1:tempi+1
-                for j = tempj-1:tempj+1
-                    if i ~= tempi && j ~= tempj && ~visited(i, j) % no self loop
-                        working = [working; table(i, j)];
-                        c1 = cost(tempi, tempj) + costmap(i, j);
-                        c2 = cost(i, j);
+        if ~visited(tempy, tempx)
+            visited(tempy, tempx) = 1;
+            % for each neighbor
+            for x = tempx:tempx+1
+                for y = tempy:tempy+1
+                    if x <= wi && y <= hi
+                        working = [working; table(y, x)];
+                        c1 = cost(tempy, tempx) + costmap(y, x);
+                        c2 = cost(y, x);
                         if c1 < c2
-                            cost(i, j) = c1;
-                            path([i, j]) = path([tempi, tempj]);
+                            cost(y, x) = c1;
+                            path(y, x, :) = path(tempy, tempx, :);
+                            index(y, x) = index(tempy, tempx) + 1;
+                            path(y, x, index(y, x)*2-1) = y;
+                            path(y, x, index(y, x)*2) = x;
                         end
                     end
                 end
-            end         
+            end
         end
     end
-    disp(path);
+%     indi = randi([2, 10]);
+%     file = num2str(indi);
+%     file = strcat(file, '.mat');
+%     file = strcat('data/path', file);
+%     load(file);
+%     xp = xx;
+%     yp = yy;
 end
-        
 
-%might want to randomize start
-
-          
+function bol = isGoal(gx, gy, goal, proportion)
+    gys = goal{1, 2} - proportion;
+    gxs = goal{1, 1} - proportion;
+    bol = gx > gxs && gy > gys;
+end
