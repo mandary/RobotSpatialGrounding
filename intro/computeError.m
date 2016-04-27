4ce0ffeaef087dae7c89a094a9e0cbe1577d7ef0
mandary <mandary@github.com>
2016-04-26 23:21:49 -0700

    learch with datas

--- intro/computeError.m
+++ intro/computeError.m
@@ -1,18 +1,13 @@
 % update weights for cost function
 % take in weights, planned path feature list, sample path feature list, 
 % learning rate, convex optimization rate(lambda)
-function updatedWeights = computeError(weights, pfeat, sfeat, lr, lambda)
+function updatedWeights = computeError(weights, gradient, lr, lambda)
     updatedWeights = weights;
     weightn = size(weights, 2);
-    siz = min(size(sfeat, 2), size(pfeat, 2));
-    for i = 1:siz
-        index = mod(i, weightn);
-        if index == 0
-            index = weightn;
-        end
-        updatedWeights(index) = updatedWeights(index) * exp(-lr * ((sfeat(i) - pfeat(i)) + lambda * updatedWeights(index)));
-        if updatedWeights(index) >= 200
-            updatedWeights(index) = 200;
+    for i = 1:weightn
+        updatedWeights(i) = updatedWeights(i) * exp(-lr * gradient(i) + lambda * updatedWeights(i));
+        if updatedWeights(i) >= 200
+            updatedWeights(i) = 200;
         end
         
     end
--- intro/learch.m
+++ intro/learch.m
@@ -1,56 +1,69 @@
 
 
 % example cost function learning algorithm
-function weights = learch(lr, iteration)
+function weights = learch(lr, iteration, lambda)
     load('data/sampleMap.mat');
-    % load data, how to concat data together? In a table?
-    load('data/path1.mat');
     % weights
-    weights = [1, 1, 1, 1];
+    weights = ones(1, 23);
     % compute feature map, number of features is always 1 less because of
     % the constant feature
-    featureMap = computeFeatureMap(width, height, goal, object, size(weights, 2) - 1);
-    % descretize line by bresenham's
-    [xs, ys] = descretize(xx, yy);
-    % compute loss map
-    lossMap = computeLossMap(width, height, xs, ys);
-    % build sample path's feature list
-    sampleFeatureList = buildList(xs, ys, featureMap);
-    sampleCost = aggregator(weights,sampleFeatureList);
-    planCost = Inf;
-    for iter = 1:iteration
-        %for sample = 1:10
-            costmap = computeCostMap(width, height, weights, featureMap);
-            costmap = lossAug(xx, yy, costmap, lossMap);
-            figure(100);
-            hold on;
-            plot(0,0);
-            imagesc(costmap); drawnow;
-            map = costmap;
-            for i = 1:height
-                for j = 1:width
-                    if A(i, j) ~= 0
-                        map(i, j) = A(i, j);
-                    end
-                end
-            end
-            figure(10);
-            hold on;
-            plot(0,0);
-            imagesc(map); drawnow;
-            disp(iter);
-            disp(weights);
-            % plan path from costmap
-            [xp, yp, planCost] = Dijkstra(costmap, start, goal, proportion);
-            [xp, yp] = descretize(xp, yp);
-            planFeatureList = buildList(xp, yp, featureMap);
-            % update weights
-            if planCost ~= sampleCost
-                weights = computeError(weights, planFeatureList, sampleFeatureList, lr, 0.1);
-            end
-            sampleCost = aggregator(weights, sampleFeatureList);
-            planCost = aggregator(weights, planFeatureList);
-        %end
-    end
+    featureMap = computeFeatureMap(width, height, goal, object, 2);
+    histFeatureMap = computeHistFeatureMap(width, height, featureMap, size(weights, 2));
     
+    for iter = 1:iteration
+        costmap = computeCostMap(width, height, weights, histFeatureMap);
+        figure(100);
+        plot(0,0);
+        hold on;
+        imagesc(costmap);
+        drawnow;
+        figure(10);
+        plot(0,0);
+        hold on;
+        imagesc(A);
+        testpath = dijkstra_grid([start{1, 1}; start{1, 2}], [goal{1, 1}; goal{1, 2}], costmap, '8-connected'); 
+        xt = testpath(1, :);
+        yt = testpath(2, :);
+        [xt, yt] = descretize(xt, yt);
+        plot(xt, yt, 'r.-');
+        gradients = zeros(1, size(weights, 2));
+        disp(iter);
+        disp(weights);
+        for sample = 1:7
+            vari = num2str(sample);
+            file = strcat(vari, '.mat');
+            file = strcat('data/path', file);
+            load(file);
+            % descretize line by bresenham's
+           % [xs, ys] = descretize(xx, yy);
+           xs = xx;
+           ys = yy;
+            % compute loss map
+            lossMap = computeLossMap(width, height, xs, ys);
+            % build sample path's feature list
+            sampleFeatureList = buildList(xs, ys, histFeatureMap);
+            tempcostmap = costmap - lossMap;
+            tempcostmap(tempcostmap < 0) = 0;
+            % plan path from costmap
+            path = dijkstra_grid([start{1, 1}; start{1, 2}], [goal{1, 1}; goal{1, 2}], tempcostmap, '8-connected'); 
+            xp = path(1, :);
+            yp = path(2, :);
+            [xp, yp] = descretize(xp, yp);
+            planFeatureList = buildList(xp, yp, histFeatureMap);         
+            siz = min(size(planFeatureList, 2), size(sampleFeatureList, 2));
+            tempgradients = zeros(1, size(weights, 2));
+            divden = zeros(1, size(weights, 2));
+            for feat = 1:siz
+                index = mod(feat, size(weights, 2));
+                if index == 0
+                    index = size(weights, 2);
+                end
+                tempgradients(index) = tempgradients(index) + sampleFeatureList(feat) - planFeatureList(feat);
+                divden(index) = divden(index) + 1;
+            end
+            gradients = gradients + (tempgradients ./ divden);
+        end
+        gradients = gradients ./ 7;
+        weights = computeError(weights, gradients, lr, lambda);
+    end
 end
